c1b6694869276cc1d7d0c617f88b5ccbb84b2ed4
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2025-01-29 18:06:36 -0300

    feat: add chat presence component to display online users

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import ChatInput from '@/components/chat-input';
 import { ChatMessages } from '@/components/chat-messages';
 import Header from '@/components/header';
+import ChatPresence from '@/components/chat-presence';
 import InitUser from '@/lib/store/initUser';
 import { supabaseServer } from '@/lib/supabase/server';
 
@@ -13,12 +14,7 @@ export default async function Home() {
       <main className="flex justify-center items-center min-h-screen p-4">
         <div className="grid grid-cols-1 sm:grid-cols-[1fr_3fr] border rounded bg-card w-full max-w-4xl h-[35rem]">
           <div className="flex flex-col h-full border-r">
-            <ul className="flex-1 hidden sm:block p-4">
-              <li>user 1</li>
-              <li>user 2</li>
-              <li>user 3</li>
-            </ul>
-
+            <ChatPresence />
             <Header />
           </div>
 
